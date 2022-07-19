e81da0b2381b59f234c8ee9e34b216ddc39b80c9
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-07-18 21:19:39 -0400

    added extra feed.

--- podcasts/management/commands/startjobs.py
+++ podcasts/management/commands/startjobs.py
@@ -6,21 +6,35 @@ from dateutil import parser
 from podcasts.models import Episode
 
 
+def save_new_episodes(feed):
+    podcast_title = feed.channel.title
+    podcast_image = feed.channel.image["href"]
+
+    for item in feed.entries:
+        if not Episode.objects.filter(guid=item.guid).exists():
+            episode = Episode(
+                title=item.title,
+                description=item.description,
+                pub_date=parser.parse(item.published),
+                link=item.link,
+                image=podcast_image,
+                podcast_name=podcast_title,
+                guid=item.guid,
+            )
+            episode.save()
+
+
+def fetch_planet_money_episodes():
+    _feed = feedparser.parse("https://feeds.npr.org/510289/podcast.xml")
+    save_new_episodes(_feed)
+
+
+def fetch_the_indicator_episodes():
+    _feed = feedparser.parse("https://feeds.npr.org/510325/podcast.xml")
+    save_new_episodes(_feed)
+
+
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        feed = feedparser.parse("https://feeds.npr.org/510289/podcast.xml")
-        podcast_title = feed.channel.title
-        podcast_image = feed.channel.image["href"]
-
-        for item in feed.entries:
-            if not Episode.objects.filter(guid=item.guid).exists():
-                episode = Episode(
-                    title=item.title,
-                    description=item.description,
-                    pub_date=parser.parse(item.published),
-                    link=item.link,
-                    image=podcast_image,
-                    podcast_name=podcast_title,
-                    guid=item.guid,
-                )
-                episode.save()
+        fetch_planet_money_episodes()
+        fetch_the_indicator_episodes()
