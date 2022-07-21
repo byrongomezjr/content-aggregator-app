717ba13f10eb5b728a9569ffa1d93af170169eed
Byron Gomez Jr <byrongomezjr@protonmail.com>
2022-07-20 22:19:17 -0400

    small changes.

--- podcastapp/settings.py
+++ podcastapp/settings.py
@@ -29,7 +29,12 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1:8000", "economicspodcasts.herokuapp.com"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1:8000",
+    "https://economicspodcasts.herokuapp.com/",
+    "127.0.0.1",
+]
 
 
 # Application definition
@@ -58,6 +63,7 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
 
 ROOT_URLCONF = "podcastapp.urls"
 
