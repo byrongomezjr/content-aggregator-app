d13bfb717e7292f7427159ef8d9711a63c55d1ba
Byron Gomez Jr <byrongomezjr@protonmail.com>
2023-02-06 20:22:37 -0500

    ran new job and modified basic views.

--- podcasts/views.py
+++ podcasts/views.py
@@ -11,5 +11,5 @@ class HomePageView(ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["episodes"] = Episode.objects.filter().order_by("-pub_date")[:10]
+        context["episodes"] = Episode.objects.filter().order_by("-pub_date")[:15]
         return context
